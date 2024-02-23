d6251549073ad87f9faa55d52917620f9f990732
adiaviad <adaviad4@gmail.com>
2024-02-23 14:02:04 +0200

    roadrunner is ass

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Placeholder.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+
+import org.firstinspires.ftc.teamcode.roadrunner.Localizer;
+
+public class Placeholder implements Localizer {
+    @Override
+    public Twist2dDual<Time> update() {
+        return null;
+    }
+}
