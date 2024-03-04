273d6e6a41479a256c490d83f3762810f89fbf75
thunderbolts <emekhefer2630@gmail.com>
2024-03-04 17:44:23 +0200

    adi did some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -96,8 +96,8 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
         m_controller2.assignCommand(m_arm.setIdle(), false, DPAD_LEFT);
         m_controller2.assignCommand(m_arm.setMiddle(), false, BUTTON_LEFT);
         m_controller2.assignCommand(m_arm.setPickup(), false, DPAD_RIGHT);
-        m_controller.assignCommand(m_chassis.goToDegrees(-100),false,DPAD_LEFT);
-        m_controller.assignCommand(testPath2(),false,DPAD_RIGHT);
+//        m_controller.assignCommand(m_chassis.goToDegrees(-100),false,DPAD_LEFT);
+//        m_controller.assignCommand(testPath2(),false,DPAD_RIGHT);
 
     }
 
